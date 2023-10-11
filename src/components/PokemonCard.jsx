a462a13890d69d582566f092ab298da3f258e298
Doriane LEVEQUE <doriane.leveque@hotmail.fr>
2023-10-11 19:59:55 +0200

    New branch added

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,22 @@
-function PokemonCard() {
-    return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption>"Bulbasaur"</figcaption>
-        </figure>
-    )
-}
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 export default PokemonCard;
+
+function PokemonCard() {
+    const pokemon = pokemonList[0];
+    return (
+        <p>
+            {pokemon === "pokemon.name" ? "pokemon.imgSrc" : "???"}
+        </p>
+    );
+}
+
