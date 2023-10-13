77f45535dd3367fa3234a7698e9e223f24b0ec0f
Doriane LEVEQUE <doriane.leveque@hotmail.fr>
2023-10-13 16:18:54 +0200

    Quest 6 - finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,58 +2,71 @@ import { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
-  const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      },
-      {
-        name: "charmander",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-      },
-      {
-        name: "squirtle",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-      },
-      {
-        name: "pikachu",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-      },
-      {
-        name: "mew",
-      },
-  ];
+	const [pokemonIndex, setPokemonIndex] = useState(0);
 
+	const pokemonList = [
+		{
+			name: "bulbasaur",
+			imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+		},
+		{
+			name: "charmander",
+			imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+		},
+		{
+			name: "squirtle",
+			imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+		},
+		{
+			name: "pikachu",
+			imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+		},
+		{
+			name: "mew",
+		},
+	];
 
- // fonction classique
+	/**
+	 * Fonction classique
+	 */
+	function pokeNext() {
+		setPokemonIndex(pokemonIndex + 1);
+	}
 
-  function pokeNext() {
-    setPokemonIndex(pokemonIndex +1);
-  }
-  // fonction fléchée
-    const pokePrev = () => {
-      setPokemonIndex(pokemonIndex -1)
-    }
+	/**
+	 * Fonction fléchée
+	 */
+	const pokePrev = () => {
+		setPokemonIndex(pokemonIndex - 1);
+	};
 
-  return (
-    <>
-      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
-      {pokemonIndex > 0 ? (
-          <button onclick={pokePrev}>Précédent</button>
-        ) : (
-          ""
-        )}
-        {pokemonIndex < pokemonList.length - 1 ? (
-          <button onclick={pokeNext}>Suivant</button>
-        ) : (
-          ""
-        )}
-    </>
-  );
+	return (
+		<>
+			<PokemonCard pokemon={pokemonList[pokemonIndex]} />
+			{/* Soluce 1 */}
+			{pokemonIndex > 0 ? (
+				<button onClick={pokePrev}>⬅️ Précédent</button>
+			) : (
+				""
+			)}
+			{pokemonIndex < pokemonList.length - 1 ? (
+				<button onClick={pokeNext}>Suivant ➡️</button>
+			) : (
+				""
+			)}
+			{/* 
+				Soluce 2
+
+				{pokemonIndex > 0 && (
+					<button onClick={pokePrev}>⬅️ Précédent</button>
+				)}
+				{pokemonIndex < pokemonList.length - 1 && (
+					<button onClick={pokeNext}>Suivant ➡️</button>
+				)}
+			
+			*/}
+		</>
+	);
 }
 
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,25 @@
 // import App from "../App";
 import PropTypes from "prop-types";
 
-PokemonCard.propTypes = {
-  pokemon: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    imgSrc: PropTypes.string,
-  }),
-};
-
 function PokemonCard({ pokemon }) {
-  console.log(typeof pokemon)
-    return (
-      <figure>
-        {pokemon.imgSrc ? (
-          <img src= {pokemon.imgSrc} alt ={pokemon.name}/> 
-        ) : (
-          <p>???</p>
-        )}
+	return (
+		<figure>
+			{pokemon.imgSrc ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>???</p>
+			)}
 
-        <figcaption>{pokemon.name}</figcaption>
-      </figure>
-    );
+			<figcaption>{pokemon.name}</figcaption>
+		</figure>
+	);
 }
+
+PokemonCard.propTypes = {
+	pokemon: PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		imgSrc: PropTypes.string,
+	}),
+};
 
 export default PokemonCard;
