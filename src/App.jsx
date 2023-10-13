4c0ae9754d4260b1744d698a7343bae61c88d338
Doriane LEVEQUE <doriane.leveque@hotmail.fr>
2023-10-13 15:59:38 +0200

    Quete - suite

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,58 @@
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
   const pokemonList = [
     {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
   ];
-  
+
+
+ // fonction classique
+
+  function pokeNext() {
+    setPokemonIndex(pokemonIndex +1);
+  }
+  // fonction fléchée
+    const pokePrev = () => {
+      setPokemonIndex(pokemonIndex -1)
+    }
+
   return (
     <>
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      {pokemonIndex > 0 ? (
+          <button onclick={pokePrev}>Précédent</button>
+        ) : (
+          ""
+        )}
+        {pokemonIndex < pokemonList.length - 1 ? (
+          <button onclick={pokeNext}>Suivant</button>
+        ) : (
+          ""
+        )
+        }
     </>
   );
 }
